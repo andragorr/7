48eeb1c133466c7938aff5fc30c3bbc05a0766a1
freefireman <beluh.c.s@gmail.com>
2020-12-22 18:51:47 +0200

    little fix)

--- src/App.js
+++ src/App.js
@@ -3,56 +3,38 @@ import Users from "./components/users/Users";
 import Posts from "./components/posts/Posts";
 import Comments from "./components/comments/Comments"
 
-import {BrowserRouter as Router, Link, Route, Switch,} from "react-router-dom";
+import {NavLink, Route, Switch,} from "react-router-dom";
 
 class App extends Component {
+
     render() {
         return (
-            <Router>
-
-            <div>
-              <ul>
-                <li>
-                  <Link to={'/users'}>
-                      to users page
-                  </Link>
-                </li>
-                  <li>
-                      <Link to={'/posts'}>
-                          to posts page
-                      </Link>
-                  </li>
-                  <li>
-                      <Link to={'/comments'}>
-                          to comments page
-                      </Link>
-                  </li>
-              </ul>
-              <div>
-              <Switch>
-                <Route path={'/users'} render={(props) => {
-                    return <Users/>;
-                }}>
-                  </Route>
-
-              </Switch>
-                  <Switch>
-                      <Route path={'/posts'} render={(props) => {
-                          return <Posts/>;
-                      }}>
-                      </Route>
-                  </Switch>
-                  <Switch>
-                      <Route path={'/comments'} render={(props) => {
-                          return <Comments/>
-                      }}>
-                      </Route>
-                  </Switch>
-
-              </div>
-
-            </div>
-            </Router>
+                <div>
+                    <ul>
+                        <li>
+                            <NavLink to={'/users'}>
+                                to users page
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to={'/posts'}>
+                                to posts page
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to={'/comments'}>
+                                to comments page
+                            </NavLink>
+                        </li>
+                    </ul>
+                    <div>
+                        <Switch>
+                            <Route path={'/users'} component={Users}/>
+                            <Route path={'/posts'} component={Posts}/>
+                            <Route path={'/comments'} component={Comments}/>
+                        </Switch>
+                    </div>
+                </div>
         );
     }
 }
