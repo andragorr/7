16f8a98e8ae739223530e1cdc5c3776184ac41db
freefireman <beluh.c.s@gmail.com>
2020-12-22 18:55:49 +0200

    import style shit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Users from "./components/users/Users";
 import Posts from "./components/posts/Posts";
 import Comments from "./components/comments/Comments"
+import './App.css'
 
 import {NavLink, Route, Switch,} from "react-router-dom";
 
