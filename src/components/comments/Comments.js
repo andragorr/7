763b6bd73567797a3e7eede3326ef1f2feab8814
andragorr <67060639+andragorr@users.noreply.github.com>
2020-12-26 13:21:15 +0200

    Revert "master"

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import Comment from "../comment/Comment";
-import {Route, withRouter} from "react-router-dom";
+import {Route, Switch, withRouter} from "react-router-dom";
 import FullComment from "../fullComment/FullComment"
 import CommentService from "../../services/CommentService";
 
@@ -8,27 +8,25 @@ class Comments extends Component {
     commentService = new CommentService();
     state = {comments:[]};
 
-    componentDidMount(){
-       this.commentService
-           .comments()
-           .then(comments=>
-               this.setState({comments})
-           )
-
+    async componentDidMount(){
+        const comments = await this.commentService.comments();
+        this.setState({comments});
     }
     render() {
         const {comments} = this.state;
-        const {match : {url}} = this.props;
+        const {match : {ulr}} = this.props;
         return (
             <div>
                 {
                 comments.map(value => <Comment item={value} key={value.id}/>)
             }
+                <Switch>
                     <Route path={url + '/:id'} render={(props) => {
                         const {match:{params:{id}}} = props;
                         return <FullComment commentId={id} key={id}/>
 
                     }}/>
+                </Switch>
             </div>
         );
     }
